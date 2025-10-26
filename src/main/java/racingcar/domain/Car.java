36ed967754c7daeb719d28f646b6b06e1d534106
kimdanha <lily034686@gmail.com>
2025-10-26 19:34:52 +0900

    feat: Car 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+public class Car {
+
+    private static final int MOVE_CONDITION = 4;
+
+    private final String name;
+    private int location = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 이내여아 합니다.");
+        }
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVE_CONDITION) {
+            location++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+}
