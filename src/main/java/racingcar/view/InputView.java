958d867ded925b315aecd929c7f64b66d2692ce9
kimdanha <lily034686@gmail.com>
2025-10-26 20:48:24 +0900

    feat: view 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
+
 public class InputView {
+
+    public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        InputValidator.validateCarNamesInput(input);
+        return input;
+    }
+
+    public static int inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        InputValidator.validateTryCountInput(input);
+        return Integer.parseInt(input);
+    }
 }
