e36b8c7181db24bf28353440eb72e824230fdcae
kimdanha <lily034686@gmail.com>
2025-10-26 21:02:12 +0900

    test: Race 테스트 추가

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -1,4 +1,28 @@
 package racingcar.domain;
 
-public class RaceTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RaceTest {
+
+    @Test
+    void 전체_자동차_이동_테스트() {
+        Race race = new Race(List.of(new Car("pobi"), new Car("woni")));
+        race.moveAll(5);
+        assertThat(race.getCars().get(0).getLocation()).isEqualTo(1);
+        assertThat(race.getCars().get(1).getLocation()).isEqualTo(1);
+    }
+
+    @Test
+    void 우승자_판별_테스트() {
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        pobi.move(5);
+        Race race = new Race(List.of(pobi, woni));
+        List<String> winners = race.findWinners();
+        assertThat(winners).containsExactly("pobi");
+    }
 }
