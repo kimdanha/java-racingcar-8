ff2a95f2ebccf1a0de4bb86d055890cdd3d8a1d0
kimdanha <lily034686@gmail.com>
2025-10-27 10:58:34 +0900

    feat: 자동차 이름 중복 검증 로직 추가

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,14 +1,26 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Race {
 
     private final List<Car> cars;
 
     public Race(List<Car> cars) {
+        validateCars(cars);
         this.cars = cars;
+    }
+
+    private void validateCars(List<Car> cars) {
+        Set<String> names = new HashSet<>();
+        for(Car car : cars) {
+            if(!names.add(car.getName())) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+        }
     }
 
     public void moveAll(int randomNumber) {
