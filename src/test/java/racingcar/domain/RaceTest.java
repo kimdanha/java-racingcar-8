a686959a9c2ab0e7103a1ff7ddd55e47a5eca905
kimdanha <lily034686@gmail.com>
2025-10-27 11:41:55 +0900

    test: 자동차 이름 중복 테스트 추가

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class RaceTest {
 
@@ -24,5 +24,14 @@ class RaceTest {
         Race race = new Race(List.of(pobi, woni));
         List<String> winners = race.findWinners();
         assertThat(winners).containsExactly("pobi");
+    }
+
+    @Test
+    void 자동차_이름_중복_테스트() {
+        Car pobi1 = new Car("pobi");
+        Car pobi2 = new Car("pobi");
+        assertThatThrownBy(() -> new Race(List.of(pobi1, pobi2)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
     }
 }
